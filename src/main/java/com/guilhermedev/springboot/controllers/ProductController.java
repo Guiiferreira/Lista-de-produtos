84d3ccdc168c7d84b97b44dbf2a1850849219c91
Guilherme Ferreira <guilhermepereiraferreira4@gmail.com>
2025-02-24 18:50:21 -0300

    Método DELETE

--- src/main/java/com/guilhermedev/springboot/controllers/ProductController.java
+++ src/main/java/com/guilhermedev/springboot/controllers/ProductController.java
@@ -20,14 +20,6 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-    // metodos CRUD
-
-    @PostMapping("/products")
-    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
-        var productModel = new ProductModel();
-        BeanUtils.copyProperties(productRecordDto, productModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
-    }
 
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts() {
@@ -43,6 +35,21 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(productO.get());
 
     }
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+}
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value="id") UUID id) {
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        productRepository.delete(productO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+    }
 
     @PutMapping("/products/{id}")
     public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
@@ -55,4 +62,7 @@ public class ProductController {
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
     }
+
+
+
 }
