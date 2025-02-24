6033196911f79b66fe48f00b5f3f2e7ced156103
Guilherme Ferreira <guilhermepereiraferreira4@gmail.com>
2025-02-24 17:38:56 -0300

    Método POST

--- src/main/java/com/guilhermedev/springboot/controllers/ProductController.java
+++ src/main/java/com/guilhermedev/springboot/controllers/ProductController.java
@@ -1,17 +1,30 @@
 package com.guilhermedev.springboot.controllers;
 
+import com.guilhermedev.springboot.dtos.ProductRecordDto;
+import com.guilhermedev.springboot.models.ProductModel;
 import com.guilhermedev.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-
 
     @Autowired
     ProductRepository productRepository;
 
     // metodos CRUD
 
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 
 }
